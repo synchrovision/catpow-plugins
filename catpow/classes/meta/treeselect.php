16335e0e78124146a54cc0129b0fb211bcd4af31
synchrovision <trinitybluster@yahoo.co.jp>
2019-11-20 14:22:50 +0900

    output,inputメソッドからコンポーネントに任意のパラメータを渡せるように修正

--- catpow/classes/meta/treeselect.php
+++ catpow/classes/meta/treeselect.php
@@ -1,15 +1,28 @@
 <?php
 namespace Catpow\meta;
 
-class treeselect extends select{
+class treeselect extends meta{
 	public static function input($meta,$prm){
         $sels=self::get_selections($meta);
-        return self::get_input($meta->the_data_path,$meta->conf,$sels,$meta->value);
+        return self::get_input($meta->the_data_path,$meta->conf,$sels,$meta->value,$prm);
+	}
+	
+	public static function get_selections($meta){
+		if(isset($meta->conf['value'])){
+        	$rtn=is_callable($meta->conf['value'])?$meta->conf['value']($meta):$meta->conf['value'];
+		}
+		else{$rtn=[];}
+        if(isset($meta->conf['addition'])){
+            if(is_array($meta->conf['addition'])){$rtn=array_merge($rtn,$meta->conf['addition']);}
+            else{$rtn[$meta->conf['addition']]=0;}
+        }
+        return $rtn;
 	}
     
-    public static function get_input($path,$conf,$sels,$val){
-		\cp::enqueue_style('ui/treeSelect/style.css');
-		\cp::enqueue_script('ui/treeSelect/component.js',['wp-element','babelHelpers']);
+    public static function get_input($path,$conf,$sels,$val,$prm){
+		\cp::enqueue_style('ui/TreeSelect/style.css');
+		\cp::enqueue_script('ui/HiddenValues/component.js',['wp-element','babelHelpers']);
+		\cp::enqueue_script('ui/TreeSelect/component.js',['ui/HiddenValues/component.js']);
 		$id=\cp::get_input_id($path);
 		ob_start();
 		?>
@@ -20,7 +33,8 @@ class treeselect extends select{
 						wp.element.createElement(TreeSelect,<?=json_encode([
 							'selections'=>$sels,
 							'value'=>$val,
-							'name'=>\cp::get_input_name($path)
+							'name'=>\cp::get_input_name($path),
+							'param'=>$prm
 						])?>),
 						document.getElementById("<?=$id?>")
 					);

4099a5ee9abb836cafa1ff63663dcd8814fd9681
synchrovision <trinitybluster@yahoo.co.jp>
2019-12-16 11:47:25 +0900

    クラス名間違い修正

--- catpow/classes/meta/post_status.php
+++ catpow/classes/meta/post_status.php
@@ -1,7 +1,7 @@
 <?php
 namespace Catpow\meta;
 
-class post_content extends post_title{
+class post_status extends post_title{
     
     public static function reflect_to_query(&$query,$data_type,$data_name,$meta_name,$id,$input,$conf){
         if(empty(array_filter($input['value']))){return false;}

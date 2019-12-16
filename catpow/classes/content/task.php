dc0cf5138bde05014dd6686794ac1879d728c004
synchrovision <trinitybluster@yahoo.co.jp>
2019-12-16 12:05:47 +0900

    inputsを親フォームと共有するように変更

--- catpow/classes/content/task.php
+++ catpow/classes/content/task.php
@@ -5,9 +5,12 @@ namespace Catpow\content;
 * 別接続からの入力を受け取るためのフォーム
 * 入力値やパラーメータをファイルとして保持し
 * トークンによってそれを共有する
+* checked=接続に対する処理許可 limitを消費する
+* complete=タスク完了フラグ
+* flag=フラグ
 */
 class task extends form{
-	public $token,$token_key,$f,$param;
+	public $valid,$token,$token_key,$f,$param;
 	
     public function __construct($param){
         parent::__construct($param);
@@ -18,11 +21,12 @@ class task extends form{
 		else{
 			$this->create();
 		}
-        if(!is_null($this->parent->form)){
+        if($this->parent && !is_null($this->parent->form)){
 			if(isset($this->parent->form->tasks[$this->form_id])){
 				$this->parent->form->tasks[$this->form_id]->delete();
 			}
 			$this->parent->form->tasks[$this->form_id]=$this;
+			$this->inputs=$this->parent->form->inputs;
 		}
     }
 	
@@ -56,19 +60,12 @@ class task extends form{
 	}
 	
 	
-	public function complete(){
-		$this->param['complete']=true;
-		$this->save();
-		return $this;
-	}
-	public function is_completed(){
-		return !empty($this->param['complete']);
-	}
 	public function create(){
 		$param=$this->param;
 		if(!isset($param['expire'])){$param['expire']=strtotime('+ 1 hour');}
 		if(!isset($param['limit'])){$param['limit']=1;}
-		if(!isset($param['authorized'])){$param['authorized']=[];}
+		if(!isset($param['checked'])){$param['checked']=[];}
+		if(!isset($param['flag'])){$param['flag']=[];}
 		if(!isset($param['complete'])){$param['complete']=false;}
 		if(!isset($param['inputs_data'])){$param['inputs_data']=[];}
 		if(!isset($param['key'])){$param['key']=\cp::rand_id(8);}
@@ -79,6 +76,7 @@ class task extends form{
 		$str="<?php\n\$param=".var_export($param,true).';';
         file_put_contents($f,$str);
 		$this->f=$f;
+		$this->valid=true;
 		$this->token=$token;
 		$this->token_key=$param['key'];
 		$this->param=$param;
@@ -87,23 +85,83 @@ class task extends form{
 		unlink($this->f);
 	}
 	public function save(){
+		if(!$this->valid){return $this;}
 		$this->param['inputs_data']=$this->inputs->data;
 		$str="<?php\n\$param=".var_export($this->param,true).';';
         file_put_contents($this->f,$str);
 		return $this;
 	}
 	public function load(){
+		if(!file_exists($this->f)){$this->valid=false;return $this;}
 		include $this->f;
-		if(in_array(\cp::$id,(array)$param['authorized'])){$this->param=$param;return $this;}
-		if($this->token_key!==$param['key']){return false;}
-		if($param['expire']<time() || $param['limit'] < 1){unlink($this->f);return false;}
-		$param['limit']--;
-		$param['authorized'][]=\cp::$id;
-		$str="<?php\n\$param=".var_export($param,true).';';
-		file_put_contents($this->f,$str);
+		if($this->token_key!==$param['key']){$this->valid=false;return $this;}
+		if($param['expire'] < time() && $param['limit'] < 1){$this->valid=false;return $this;}
+		$this->valid=true;
 		$this->param=$param;
 		$this->inputs->data=$this->param['inputs_data'];
 		return $this;
+	}
+	
+	public function check(){
+		if($this->checked()){return true;}
+		$this->load();
+		if(!$this->valid){return false;}
+		$this->param['checked'][\cp::$id]=true;
+		$this->param['limit']--;
+		$this->save();
+		return true;
+	}
+	public function checked(){
+		return isset($this->param['checked'][\cp::$id]);
+	}
+	
+	public function complete(){
+		$this->param['complete']=true;
+		$this->save();
+		return $this;
+	}
+	public function is_completed(){
+		return !empty($this->param['complete']);
+	}
+	public function wait_complete(){
+		while(true){
+			if($this->load()->is_completed()){return true;}
+			sleep(1);
+		}
+	}
+	
+	/**
+	* アクセスされると任意のフラグを立てるビーコンを出力
+	* @param $flag string フラグ名
+	*/
+	public function beacon($flag='beacon'){
+		$key=\cp::rand_id(8);
+		$this->param['beacon'][$key]=$flag;
+		$this->save();
+		printf(
+			'<img src="%s" alt="" width="0" height="0"/>',
+			plugins_url('catpow/callee/beacon.php').'?'.http_build_query([
+				'path'=>get_current_blog_id().'/'.substr($this->file_path,0,-4).'/'.$this->token.'-'.$this->token_key,
+				'key'=>$key
+			])
+		);
+		return true;
+	}
+	
+	public function flag($name,$val=null){
+		if(isset($val)){
+			if(empty($val)){unset($this->param['flag'][$name]);}
+			else{$this->param['flag'][$name]=true;}
+			return $this;
+		}
+		return isset($this->param['flag'][$name]);
+	}
+	public function wait_flag($name){
+		while($this->valid){
+			if($this->load()->flag($name)){return true;}
+			sleep(1);
+		}
+		return false;
 	}
 }
 

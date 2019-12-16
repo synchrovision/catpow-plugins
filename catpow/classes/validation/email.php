6cbecaf800783f2c3e695d51f9b11e2274e400d7
synchrovision <trinitybluster@yahoo.co.jp>
2019-12-16 11:58:27 +0900

    翻訳関数の間違いを修正

--- catpow/classes/validation/email.php
+++ catpow/classes/validation/email.php
@@ -5,7 +5,7 @@ class email extends pattern{
     public static $pattern='/^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/';
 	
 	public static function get_message($meta){
-		return _('メールアドレスを入力してください');
+		return __('メールアドレスを入力してください','catpow');
 	}
 }
 

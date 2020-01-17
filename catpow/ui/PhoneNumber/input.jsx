3b00907c2b6d49ab9b78b912eaada8d03671bd19
synchrovision <trinitybluster@yahoo.co.jp>
2020-01-17 13:37:37 +0900

    PhoneNumberUIを追加

---
+++ catpow/ui/PhoneNumber/input.jsx
@@ -0,0 +1,60 @@
+﻿Catpow.PhoneNumber=class extends wp.element.Component{
+	constructor(props) {
+		super(props);
+		this.secs0=React.createRef();
+		this.secs1=React.createRef();
+		this.secs2=React.createRef();
+		this.state={value:props.value};
+	}
+	render(){
+		var {value}=this.state;
+		var secs=value.split('-');
+		
+		const input=(i)=>(
+			<input
+				type="text"
+				className={"sec"+i}
+				size="4"
+				autocomplete={['tel-area-code','tel-local-prefix','tel-local-suffix'][i]}
+				onChange={(e)=>{
+					var val=e.target.value;
+					if(!val.match(/^\d+$/)){val='';}
+					if(val.length===10){
+						secs[0]=val.substring(0,2);
+						secs[1]=val.substring(2,6);
+						secs[2]=val.substring(6);
+					}
+					else if(val.length===11){
+						secs[0]=val.substring(0,3);
+						secs[1]=val.substring(3,7);
+						secs[2]=val.substring(7);
+					}
+					else{
+						secs[i]=val;
+						if((val.length>3 && i<2) || (i==0 && val.match(/^0\d0$/))){
+							this['secs'+(i+1)].current.focus();
+						}
+					}
+					this.setState({value:secs.join('-')});
+				}}
+				ref={this['secs'+i]}
+				value={secs[i]}
+			/>
+		);
+		
+		return (
+			<div className={'PhoneNumber'}>
+				{input(0)}
+				<span class="sep">-</span>
+				{input(1)}
+				<span class="sep">-</span>
+				{input(2)}
+
+				<Catpow.HiddenValues
+					name={this.props.name}
+					value={value}
+				/>
+			</div>
+		);
+	}
+}

b33d9dd7534d3dc66fae66894b24fb96432b6489
synchrovision <trinitybluster@yahoo.co.jp>
2020-01-17 13:22:11 +0900

    markdownブロックを削除

--- catpow/blocks/markdown/conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-$attributes=[
-	'classes'=>['type'=>'string','default'=>'wp-block-catpow-markdown document'],
-	'code'=>['type'=>'string','default'=>"MarkDown\n--"]
-];
--- catpow/blocks/markdown/render.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<div class="'.$attr['classes'].'">';
-echo Michelf\MarkdownExtra::defaultTransform($attr['code']);
-echo '</div>';

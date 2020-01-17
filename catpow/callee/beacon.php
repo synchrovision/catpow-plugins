35922f789a7bf676ede8925e6d6799b4a89f3476
synchrovision <trinitybluster@yahoo.co.jp>
2020-01-17 13:44:08 +0900

    ビーコンで、カウント、指定のフラグを立てる、一回のみ処理、を可能に

--- catpow/callee/beacon.php
+++ catpow/callee/beacon.php
@@ -13,7 +13,11 @@ if(preg_match(
 	if(!file_exists($f)){die();}
 	include $f;
 	if(empty($param['beacon'][$_GET['key']])){die();}
-	$param['flag'][$param['beacon'][$_GET['key']]]=true;
-	unset($param['beacon'][$_GET['key']]);
+	$act=$param['beacon'][$_GET['key']];
+	$name=key($act);
+	$flag=(int)reset($act);
+	if($flag&1){$param['count'][$name]++;}
+	else{$param['flag'][$name]=true;}
+	if(!$flag&2){unset($param['beacon'][$_GET['key']]);}
 	file_put_contents($f,"<?php\n\$param=".var_export($param,true).';');
 };

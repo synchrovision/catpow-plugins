034d638d6dbc3c225081a2c8657cc6de6ec92b2d
synchrovision <trinitybluster@yahoo.co.jp>
2020-01-18 19:43:17 +0900

    不要なコードを削除

--- catpow/classes/conf.php
+++ catpow/classes/conf.php
@@ -3,39 +3,8 @@ namespace Catpow;
 
 class conf implements \ArrayAccess{
 	public $data;
-	public function __construct($csv,$fill_column=false){
-		if(is_array($csv)){
-			$this->data=$csv;
-		}
-		else{
-			$this->file=$csv;
-			$csv=fopen($csv,'r');
-			if(empty($fill_coumn)){
-				while($row=fgetcsv($csv)){
-					array_push($this->data,$row);
-				}
-			}
-			else{
-				$keys=fgetcsv($csv);
-				array_push($this->data,$keys);
-				if($fill_column===true){$fill_column=count($keys);}
-				if(is_numeric($fill_column)){$fill_column=range(0,$fill_column);}
-				elseif(is_string($fill_column)){$fill_column=[array_search($fill_column,$keys)];}
-				elseif(is_array($fill_column)){
-					foreach($fill_column as $i=>$key){
-						if(!is_numeric($key)){$fill_column[$i]=array_search($key,$keys);}
-					}
-				}
-				$current_values=[];
-				while($row=fgetcsv($csv)){
-					foreach($fill_column as $index){
-						if(empty($row[$index])){$row[$index]=$current_values[$index];}
-						else{$current_values[$index]=$row[$index];}
-					}
-					array_push($this->data,$row);
-				}
-			}
-		}
+	public function __construct($data){
+		$this->data=$data;
 	}
 	
 	public function offsetSet($offset,$value){

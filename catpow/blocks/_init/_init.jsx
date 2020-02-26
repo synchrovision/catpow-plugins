37641f7f3f306640b58f6d7e7eca6920dab04fed
synchrovision <trinitybluster@yahoo.co.jp>
2020-02-26 18:36:24 +0900

    非推奨のコードを修正

--- catpow/blocks/_init/_init.jsx
+++ catpow/blocks/_init/_init.jsx
@@ -1,7 +1,7 @@
 const {Fragment} = wp.element;
-const {registerBlockType} = wp.blocks;
-const {InnerBlocks,BlockControls,AlignmentToolbar,BlockAlignmentToolbar,BlockVerticalAlignmentToolbar,PanelColorSettings,InspectorControls,RichText,RichTextToolbarButton,RichTextShortcut} = wp.editor;
-const {PanelBody,BaseControl,TreeSelect,TextareaControl,TextControl,ServerSideRender,ToggleControl,SelectControl,CheckboxControl,RangeControl,Button,Toolbar,FormFileUpload} = wp.components;
+const {registerBlockType,createBlock} = wp.blocks;
+const {InnerBlocks,BlockControls,AlignmentToolbar,BlockAlignmentToolbar,BlockVerticalAlignmentToolbar,PanelColorSettings,InspectorControls,RichText,RichTextToolbarButton,RichTextShortcut} = wp.blockEditor;
+const {PanelBody,BaseControl,TreeSelect,TextareaControl,TextControl,ServerSideRender,ToggleControl,SelectControl,CheckboxControl,RadioControl,RangeControl,Button,Toolbar,FormFileUpload,Icon} = wp.components;
 const {registerFormatType,toggleFormat}=wp.richText;
 const {__}=wp.i18n;
 const el=wp.element.createElement;

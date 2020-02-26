69364b86b12140500a0c699b87a45cef7b6fe648
synchrovision <trinitybluster@yahoo.co.jp>
2020-02-26 19:03:43 +0900

    formブロックでフォームの入力名およびデータIDを指定可能に

--- catpow/blocks/form/conf.php
+++ catpow/blocks/form/conf.php
@@ -3,5 +3,7 @@ $attributes=[
 	'className'=>['type'=>'string'],
 	'content_path'=>['type'=>'string'],
 	'post_data_path'=>['type'=>'string'],
-    'inputs'=>['type'=>'string']
+    'inputs'=>['type'=>'string'],
+    'data_id'=>['type'=>'string'],
+    'values'=>['type'=>'string']
 ];

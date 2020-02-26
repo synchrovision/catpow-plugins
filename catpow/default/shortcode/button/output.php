470702619377f2ddc8719f2dcfe166c3fb9fca6b
synchrovision <trinitybluster@yahoo.co.jp>
2020-02-26 19:45:06 +0900

    formプロパティ未定義判定でgetterによる取得を考慮するように修正

--- catpow/default/shortcode/button/output.php
+++ catpow/default/shortcode/button/output.php
@@ -8,7 +8,7 @@ $prm=shortcode_atts([
 	'ignore_message'=>null
 ],$atts);
 if(isset($prm['param'])){$prm['param']=json_decode($prm['param']);}
-if(!empty(\cp::$content->form)){
+if(!is_null(\cp::$content->form)){
 	\cp::$content->form->button($prm[0],$prm[1],$prm['param'],$prm['target'],$prm['ignore_message']);
 }
 else{

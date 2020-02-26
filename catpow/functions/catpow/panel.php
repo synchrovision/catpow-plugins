7f1e9981c13c9c7761c4e8584e166f5d63d2a361
synchrovision <trinitybluster@yahoo.co.jp>
2020-02-26 19:41:26 +0900

    Catpowの管理画面の構文とスタイルをブロックと共通化

--- catpow/functions/catpow/panel.php
+++ catpow/functions/catpow/panel.php
@@ -2,12 +2,12 @@
 <ul>
     <li>
         <h3>テーマ</h3>
-        <ul class="buttons">
-            <li class="refresh">
+        <ul class="wp-block-catpow-buttons s i">
+            <li class="item refresh">
                 <?php button('新規テーマ作成','action','update_results',['setup_type'=>'create']);?>
                 <small>新規のcatpowテーマを作成します</small>
             </li>
-            <li class="refresh">
+            <li class="item refresh">
                 <?php button('テンプレート生成','action','update_results',['setup_type'=>'template']);?>
                 <small>
                     config/system_config.phpの設定を元にテーマにテンプレートファイルを生成します。
@@ -17,16 +17,16 @@
     </li>
     <li>
         <h3>データ</h3>
-        <ul class="buttons">
-            <li class="refresh">
+        <ul class="wp-block-catpow-buttons s i">
+            <li class=item "refresh">
                 <?php button('投稿生成','action','update_results',['setup_type'=>'post']);?>
                 <small>使用するテンプレートタイプを元に投稿を生成します</small>
             </li>
-            <li class="refresh">
+            <li class="item refresh">
                 <?php button('ロール生成','action','update_results',['setup_type'=>'user']);?>
                 <small>config/system_config.phpの$user_datasの設定を元にユーザのロールを更新します</small>
             </li>
-            <li class="refresh">
+            <li class="item refresh">
                 <?php button('メニュー生成','action','update_results',['setup_type'=>'menu']);?>
                 <small>config/system_config.phpの$menu_datasの設定を元にメニューを更新します</small>
             </li>
@@ -34,8 +34,8 @@
     </li>
     <li>
         <h3>データベース</h3>
-        <ul class="buttons">
-            <li class="refresh">
+        <ul class="wp-block-catpow-buttons s i">
+            <li class="item refresh">
                 <?php button('データベース更新','action','update_results',['setup_type'=>'db']);?>
                 <small>
                     system_config.phpの設定を元にデータベーステーブルを生成・削除・更新します。

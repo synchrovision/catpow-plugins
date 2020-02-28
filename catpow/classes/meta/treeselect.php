3b89273643e97c719016c18264e7a656148bfb67
synchrovision <trinitybluster@yahoo.co.jp>
2020-02-28 14:26:26 +0900

    不要なコードを削除

--- catpow/classes/meta/treeselect.php
+++ catpow/classes/meta/treeselect.php
@@ -1,7 +1,7 @@
 <?php
 namespace Catpow\meta;
 
-class treeselect extends meta{
+class TreeSelect extends UI{
 	public static function input($meta,$prm){
         $sels=self::get_selections($meta);
         return self::get_input($meta->the_data_path,$meta->conf,$sels,$meta->value,$prm);
@@ -18,31 +18,5 @@ class treeselect extends meta{
         }
         return $rtn;
 	}
-    
-    public static function get_input($path,$conf,$sels,$val,$prm){
-		\cp::enqueue_style('ui/TreeSelect/style.css');
-		\cp::enqueue_script('ui/HiddenValues/component.js',['wp-element','babelHelpers']);
-		\cp::enqueue_script('ui/TreeSelect/component.js',['ui/HiddenValues/component.js']);
-		$id=\cp::get_input_id($path);
-		ob_start();
-		?>
-		<div<?=\cp::get_item_attr($path,$conf)?>>
-			<script type="text/javascript">
-				jQuery(function($){
-					wp.element.render(
-						wp.element.createElement(TreeSelect,<?=json_encode([
-							'selections'=>$sels,
-							'value'=>$val,
-							'name'=>\cp::get_input_name($path),
-							'param'=>$prm
-						])?>),
-						document.getElementById("<?=$id?>")
-					);
-				});
-			</script>
-		</div>
-		<?php
-        return ob_get_clean();
-    }
 }
 ?>

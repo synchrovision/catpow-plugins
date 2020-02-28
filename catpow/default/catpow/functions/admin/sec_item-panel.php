9891fb58463f14ffc9349c0501cf440d1401b0af
synchrovision <trinitybluster@yahoo.co.jp>
2020-02-28 14:30:20 +0900

    buttonsブロックに合わせたクラスを付与

--- catpow/default/catpow/functions/admin/sec_item-panel.php
+++ catpow/default/catpow/functions/admin/sec_item-panel.php
@@ -3,6 +3,6 @@ namespace Catpow;
 if($f=cp::get_file_path('functions/'.this()->data_name.'/panel.php')){include $f;}
 §results();
 ?>
-<ul class="buttons cp_lightbox_control">
-    <li class="close"><?php button('','close','lightbox_close'); ?></li>
+<ul class="wp-block-catpow-buttons">
+	<li class="item close"><?php button('','close','lightbox_close'); ?></li>
 </ul>

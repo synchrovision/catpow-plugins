d7b4c078f25691ea9bad3e74edeffc44cf9c8aa9
synchrovision <trinitybluster@yahoo.co.jp>
2020-04-03 16:43:44 +0900

    feat :+1: output DateSelect as date

--- catpow/classes/meta/DateSelect.php
+++ catpow/classes/meta/DateSelect.php
@@ -2,6 +2,7 @@
 namespace Catpow\meta;
 
 class DateSelect extends UI{
+	public static $output_type='date';
 	public static function fill_param($prm,$conf){
 		$prm=(array)$prm;
 		if(isset($conf['min'])){$prm['min']=date('Y-m-d',strtotime($conf['min']));}

faf42c3616f19780bca30dc2afa8e6c86c8be4bc
synchrovision <trinitybluster@yahoo.co.jp>
2020-04-09 18:03:49 +0900

    feat :+1: add default value

--- catpow/classes/meta/limit.php
+++ catpow/classes/meta/limit.php
@@ -2,8 +2,13 @@
 namespace Catpow\meta;
 
 class limit extends number{
+	static $input_type='number';
 	public static function reflect_to_query(&$query,$data_type,$data_name,$meta_name,$id,$input,$conf){
 		$query['limit']=$input['value'][0];
+	}
+	public static function fill_conf(&$conf){
+		$conf['min']=$conf['min']??1;
+		$conf['max']=$conf['max']??999;
 	}
 }
 ?>

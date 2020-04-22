b49be416aa3ad575a7806cb1836f221a1069dd4e
synchrovision <trinitybluster@yahoo.co.jp>
2020-04-22 18:46:30 +0900

    feat :+1: add defaultLabel

--- catpow/classes/meta/treeselect.php
+++ catpow/classes/meta/treeselect.php
@@ -2,15 +2,14 @@
 namespace Catpow\meta;
 
 class TreeSelect extends UI{
-	static $ui='TreeSelect';
+	static $ui='TreeSelect',$output_type='select',$defaultParam=['defaultLabel'=>'---'];
 	public static function fill_param($prm,$meta){
-		$prm=parent::fill_param($prm,$conf);
+		$prm=parent::fill_param($prm,$meta);
 		$prm['selections']=static::get_selections($meta);
 		return $prm;
 	}
-	
 	public static function get_selections($meta){
-		return select::get_selections($meta);
+		return ('Catpow\\meta\\'.static::$output_type)::get_selections($meta);
 	}
 }
 ?>

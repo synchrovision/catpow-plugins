5eb479d8665673d08c29bb5fd270b6b69baaa9a9
synchrovision <trinitybluster@yahoo.co.jp>
2020-04-22 12:05:10 +0900

    feat :+1: change default template

--- catpow/default/config/template/admin/search.php
+++ catpow/default/config/template/admin/search.php
@@ -1,2 +1,9 @@
 <?php namespace Catpow; ?>
-<!--inputs_search-->
+<table class="inputs">
+	<!--meta:can_search-->
+	<tr>
+		<th><!--label--></th>
+		<td><!--@input_search {$name}--></td>
+	</tr>
+	<!--/meta-->
+</table>

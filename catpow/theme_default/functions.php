6175d6bb550ec4d3c3baa88b58709abe85d50ff5
synchrovision <trinitybluster@yahoo.co.jp>
2020-05-13 19:20:00 +0900

    chore :up: update to latest

--- catpow/theme_default/functions.php
+++ catpow/theme_default/functions.php
@@ -4,16 +4,11 @@ if(class_exists('cp')){
 	/*管理バー*/
 	if(!current_user_can('administrator')){show_admin_bar(false);}
 
-
 	/*カスタマイズ機能*/
 	add_theme_support('title-tag');
-	add_theme_support('custom-header',['default-image'=>cp::get_file_url('/images/header.jpg'),'height'=>'100px','width'=>'600px']);
+	add_theme_support('custom-header',['default-image'=>cp::get_file_url('/images/header.jpg'),'height'=>'1080px','width'=>'1920px']);
 	add_theme_support('custom-logo',['default-image'=>cp::get_file_url('/images/logo.png')]);
 	add_theme_support('custom-background',['default-image'=>cp::get_file_url('/images/bg.png')]);
-
-	/*通知メール*/
-	add_filter('wp_mail_from_name',function($name){return get_option('blogname');});
-	add_filter('wp_mail_from',function($email){return get_option('admin_email');});
 
 
 	/*管理画面アクセス制限*/
@@ -31,5 +26,15 @@ if(class_exists('cp')){
 		wp_enqueue_script('cp_article_nav');
 	});
 	
-	add_image_size('sp',640);
+	/*カスタムカラー無効*/
+	add_theme_support('editor-color-palette');
+	add_theme_support('disable-custom-colors');
+	
+	/*カスタムフォントサイズ無効*/
+	add_theme_support('editor-font-sizes',[
+		['name'=>'大','slug'=>'large','size'=>'1.6rem'],
+		['name'=>'中','slug'=>'regular','size'=>'1.3rem'],
+		['name'=>'小','slug'=>'small','size'=>'1rem'],
+	]);
+	add_theme_support('disable-custom-font-sizes');
 }

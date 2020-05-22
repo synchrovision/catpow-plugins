a9e5c7ffeb63e99c24e4470b572c5537d95aea5a
synchrovision <trinitybluster@yahoo.co.jp>
2020-05-22 12:08:12 +0900

    feat :+1: use shortcode param as query param

--- catpow/default/shortcode/loop/output.php
+++ catpow/default/shortcode/loop/output.php
@@ -1,13 +1,13 @@
 <?php
 namespace Catpow;
-$prm=shortcode_atts(array(0=>false,1=>null),$atts);
+$prm=shortcode_atts(array(0=>false),$atts);
 
 if(cp::$content){
 	if(empty($content)){
-		loop($prm[0],$prm[1]);
+		loop($prm[0],$atts);
 	}
 	else{
-		foreach(loop($prm[0],$prm[1]) as $obj){
+		foreach(loop($prm[0],$atts) as $obj){
 			echo do_shortcode($content);
 		}
 	}

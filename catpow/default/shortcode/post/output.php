9527eff0b2629b94c5c8e79408f5e219ea3d9f05
synchrovision <trinitybluster@yahoo.co.jp>
2020-05-22 12:10:50 +0900

    fix :bug: prevent infinit loop

--- catpow/default/shortcode/post/output.php
+++ catpow/default/shortcode/post/output.php
@@ -4,10 +4,10 @@ $prm=shortcode_atts([0=>'name'],$atts);
 switch($prm[0]){
 	case 'id':the_ID();break;
 	case 'title':the_title();break;
-	case 'content':the_content();break;
+	case 'content':echo do_blocks($GLOBALS['post']->post_content);break;
+	case 'excerpt':the_excerpt();break;
 	case 'date':the_date();break;
 	case 'author':the_author();break;
 	case 'url':the_permalink();break;
-	default:
-		echo the_permalink();break;
+	default: echo $GLOBALS['post']->{$prm[0]};
 }

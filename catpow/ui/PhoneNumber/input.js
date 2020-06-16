f620e52daa4eb4af666ce91731d1299f34613998
synchrovision <trinitybluster@yahoo.co.jp>
2020-06-16 11:24:41 +0900

    fix :bug: fix behavior on input with composing

--- catpow/ui/PhoneNumber/input.js
+++ catpow/ui/PhoneNumber/input.js
@@ -9,45 +9,61 @@ Catpow.PhoneNumber = function (_wp$element$Component) {
 		_this.secs0 = React.createRef();
 		_this.secs1 = React.createRef();
 		_this.secs2 = React.createRef();
-		_this.state = { value: props.value };
+		_this.state = { value: props.value, isComposing: false };
 		return _this;
 	}
 
 	babelHelpers.createClass(_class, [{
-		key: "render",
+		key: 'render',
 		value: function render() {
 			var _this2 = this;
 
-			var value = this.state.value;
+			var _state = this.state,
+			    value = _state.value,
+			    isComposing = _state.isComposing;
 
 			var secs = value.split('-');
 
+			var setValue = function setValue(i, val) {
+				if (!val.match(/^\d+$/)) {
+					val = '';
+				}
+				if (val.length === 10) {
+					secs[0] = val.substring(0, 2);
+					secs[1] = val.substring(2, 6);
+					secs[2] = val.substring(6);
+				} else if (val.length === 11) {
+					secs[0] = val.substring(0, 3);
+					secs[1] = val.substring(3, 7);
+					secs[2] = val.substring(7);
+				} else {
+					secs[i] = val;
+					if (val.length > 3 && i < 2 || i == 0 && val.match(/^0\d0$/)) {
+						if (!isComposing) {
+							_this2['secs' + (i + 1)].current.focus();
+						}
+					}
+				}
+				_this2.setState({ value: secs.join('-') });
+			};
+
 			var input = function input(i) {
-				return wp.element.createElement("input", {
-					type: "text",
+				return wp.element.createElement('input', {
+					type: 'text',
 					className: "sec" + i,
-					size: "4",
+					size: '4',
 					autocomplete: ['tel-area-code', 'tel-local-prefix', 'tel-local-suffix'][i],
 					onChange: function onChange(e) {
 						var val = e.target.value;
-						if (!val.match(/^\d+$/)) {
-							val = '';
-						}
-						if (val.length === 10) {
-							secs[0] = val.substring(0, 2);
-							secs[1] = val.substring(2, 6);
-							secs[2] = val.substring(6);
-						} else if (val.length === 11) {
-							secs[0] = val.substring(0, 3);
-							secs[1] = val.substring(3, 7);
-							secs[2] = val.substring(7);
-						} else {
-							secs[i] = val;
-							if (val.length > 3 && i < 2 || i == 0 && val.match(/^0\d0$/)) {
-								_this2['secs' + (i + 1)].current.focus();
-							}
-						}
-						_this2.setState({ value: secs.join('-') });
+						setValue(i, e.target.value);
+					},
+					onCompositionStart: function onCompositionStart(e) {
+						_this2.setState({ isComposing: true });
+					},
+					onCompositionEnd: function onCompositionEnd(e) {
+						isComposing = false;
+						setValue(i, e.target.value);
+						_this2.setState({ isComposing: isComposing });
 					},
 					ref: _this2['secs' + i],
 					value: secs[i]
@@ -55,19 +71,19 @@ Catpow.PhoneNumber = function (_wp$element$Component) {
 			};
 
 			return wp.element.createElement(
-				"div",
+				'div',
 				{ className: 'PhoneNumber' },
 				input(0),
 				wp.element.createElement(
-					"span",
-					{ "class": "sep" },
-					"-"
+					'span',
+					{ 'class': 'sep' },
+					'-'
 				),
 				input(1),
 				wp.element.createElement(
-					"span",
-					{ "class": "sep" },
-					"-"
+					'span',
+					{ 'class': 'sep' },
+					'-'
 				),
 				input(2),
 				wp.element.createElement(Catpow.HiddenValues, {
--- catpow/ui/PhoneNumber/input.jsx
+++ catpow/ui/PhoneNumber/input.jsx
@@ -4,11 +4,34 @@
 		this.secs0=React.createRef();
 		this.secs1=React.createRef();
 		this.secs2=React.createRef();
-		this.state={value:props.value};
+		this.state={value:props.value,isComposing:false};
 	}
 	render(){
-		var {value}=this.state;
+		var {value,isComposing}=this.state;
 		var secs=value.split('-');
+		
+		const setValue=(i,val)=>{
+			if(!val.match(/^\d+$/)){val='';}
+			if(val.length===10){
+				secs[0]=val.substring(0,2);
+				secs[1]=val.substring(2,6);
+				secs[2]=val.substring(6);
+			}
+			else if(val.length===11){
+				secs[0]=val.substring(0,3);
+				secs[1]=val.substring(3,7);
+				secs[2]=val.substring(7);
+			}
+			else{
+				secs[i]=val;
+				if((val.length>3 && i<2) || (i==0 && val.match(/^0\d0$/))){
+					if(!isComposing){
+						this['secs'+(i+1)].current.focus();
+					}
+				}
+			}
+			this.setState({value:secs.join('-')});
+		};
 		
 		const input=(i)=>(
 			<input
@@ -18,24 +41,15 @@
 				autocomplete={['tel-area-code','tel-local-prefix','tel-local-suffix'][i]}
 				onChange={(e)=>{
 					var val=e.target.value;
-					if(!val.match(/^\d+$/)){val='';}
-					if(val.length===10){
-						secs[0]=val.substring(0,2);
-						secs[1]=val.substring(2,6);
-						secs[2]=val.substring(6);
-					}
-					else if(val.length===11){
-						secs[0]=val.substring(0,3);
-						secs[1]=val.substring(3,7);
-						secs[2]=val.substring(7);
-					}
-					else{
-						secs[i]=val;
-						if((val.length>3 && i<2) || (i==0 && val.match(/^0\d0$/))){
-							this['secs'+(i+1)].current.focus();
-						}
-					}
-					this.setState({value:secs.join('-')});
+					setValue(i,e.target.value);
+				}}
+				onCompositionStart={(e)=>{
+					this.setState({isComposing:true});
+				}}
+				onCompositionEnd={(e)=>{
+					isComposing=false;
+					setValue(i,e.target.value);
+					this.setState({isComposing});
 				}}
 				ref={this['secs'+i]}
 				value={secs[i]}

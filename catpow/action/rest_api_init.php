26e18ee0a604bc17680a22e98c7304c3b075084d
synchrovision <trinitybluster@yahoo.co.jp>
2020-06-28 11:53:07 +0900

    feat :+1:  make adding API EndPoint easier

--- catpow/action/rest_api_init.php
+++ catpow/action/rest_api_init.php
@@ -1,30 +1,30 @@
 <?php
 register_rest_route(
 	'cp/v1',
-	'/(?P<content_path>(?P<data_type>\w+)/(?P<data_name>[\w_]+)/(?P<tmp>(?P<tmp_name>[\w_]+)(\-(?P<tmp_slug>[\w_]+))?)/)(?P<rest_name>[\w_]+)(/(?P<param>.+))?/?',
+	'/(?P<content_path>(?P<data_type>[a-z]\w*)(?:/(?P<data_name>[a-z]\w*))?(?:/(?P<tmp>(?P<tmp_name>[a-z]\w*)(?:\-(?P<tmp_slug>[a-z]\w*))?))?)(?:/(?P<data_id>\d+))?(?:/(?P<action>[a-z]\w*))?(?:/(?P<param>.+))?/?',
 	[
 		'methods'=>WP_REST_Server::ALLMETHODS,
 		'callback'=>function($req){
-			$res_404=new WP_REST_Response(['message'=>__('無効なリクエストです','catpow')],404);
-			$conf=$GLOBALS[cp::get_conf_data_name($req['data_type'])][$req['data_name']]?:null;
-			if(!isset($conf)){return $res_404;}
-
-			$query_class=cp::get_class_name('query',$req['data_type']);
-			if(!in_array($req['tmp'],$conf[$query_class::$is_meta?'alias_template':'template'])){return $res_404;}
-
-			$tmp_class=cp::get_class_name('template_type',$req['tmp_name']);
-			$rest_routes=$tmp_class::get_rest_routes($conf);
-			if(empty($f=$rest_routes[$req['rest_name']])){return $res_404;}
-
+			$res=new WP_REST_Response([],200);
 			try{
-				$res=new WP_REST_Response([],200);
-				if(is_callable($f)){$f($req,$res);}
-				elseif(cp::get_template_part($req['content_path'].$f,['req'=>$req,'res'=>$res])===false){return $res_404;}
+				if(!empty($req['tmp'])){
+					$conf=$GLOBALS[cp::get_conf_data_name($req['data_type'])][$req['data_name']]??null;
+					if(
+						isset($conf) && 
+						(in_array($req['tmp'],$conf['template']??[]) || in_array($req['tmp'],$conf['alias_template']??[])) &&
+						cp::get_template_part($req['content_path'].'/api.php',['req'=>$req,'res'=>$res,'conf'=>$conf])
+					){return $res;}
+				}
+				$api_class=cp::get_class_name('api',$req['data_type'],$req['data_name']);
+				if(class_exists($api_class)){
+					$api_class::call($req,$res);
+					return $res;
+				}
 			}
 			catch(Exception $e){
 				return new WP_REST_Response(['message'=>$e->getMessage()],500);
 			}
-			return $res;
+			return new WP_REST_Response(['message'=>__('無効なリクエストです','catpow')],404);
 		}
 	]
 );

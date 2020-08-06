e1fc4b892bf2abb1200b5e3225760c969361657e
synchrovision <trinitybluster@yahoo.co.jp>
2020-08-06 18:45:26 +0900

    feat :+1: add loopblock block

---
+++ catpow/blocks/loopblock/editor_script.js
@@ -0,0 +1,52 @@
+registerBlockType('catpow/loopblock', {
+	title: '🐾 Loopblock',
+	icon: 'editor-code',
+	category: 'catpow',
+	edit: function edit(_ref) {
+		var attributes = _ref.attributes,
+		    setAttributes = _ref.setAttributes,
+		    className = _ref.className;
+		var content_path = attributes.content_path,
+		    query = attributes.query;
+
+
+		return [wp.element.createElement(
+			'div',
+			{ 'class': 'embedded_content' },
+			wp.element.createElement(
+				'div',
+				{ 'class': 'label' },
+				content_path
+			),
+			wp.element.createElement(InnerBlocks, null)
+		), wp.element.createElement(
+			InspectorControls,
+			null,
+			wp.element.createElement(
+				PanelBody,
+				{ title: 'Query' },
+				wp.element.createElement(TextControl, {
+					label: 'content path',
+					value: content_path,
+					onChange: function onChange(query) {
+						setAttributes({ content_path: content_path });
+					}
+				}),
+				wp.element.createElement(TextareaControl, {
+					label: 'query',
+					value: query,
+					onChange: function onChange(query) {
+						setAttributes({ query: query });
+					}
+				})
+			)
+		)];
+	},
+	save: function save(_ref2) {
+		var attributes = _ref2.attributes,
+		    className = _ref2.className,
+		    setAttributes = _ref2.setAttributes;
+
+		return wp.element.createElement(InnerBlocks.Content, null);
+	}
+});
---
+++ catpow/blocks/loopblock/editor_script.jsx
@@ -0,0 +1,33 @@
+﻿registerBlockType('catpow/loopblock',{
+	title: '🐾 Loopblock',
+	icon: 'editor-code',
+	category: 'catpow',
+	edit({attributes,setAttributes,className}){
+        const {content_path,query}=attributes;
+		 
+        return [
+			<div class="embedded_content">
+				<div class="label">{content_path}</div>
+				<InnerBlocks/>
+			</div>,
+			<InspectorControls>
+				<PanelBody title="Query">
+					<TextControl
+						label='content path'
+						value={content_path}
+						onChange={(query)=>{setAttributes({content_path});}}
+					/>
+					<TextareaControl
+						label='query'
+						value={query}
+						onChange={(query)=>{setAttributes({query});}}
+					/>
+				</PanelBody>
+			</InspectorControls>
+        ];
+    },
+
+	save({attributes,className,setAttributes}){
+		return <InnerBlocks.Content/>;
+	}
+});

0961f7c7335f8d0bee45da26b89ef8b0ffb41204
synchrovision <trinitybluster@yahoo.co.jp>
2020-08-06 18:47:51 +0900

    feat :+1:  enable build query from function

--- catpow/blocks/loop/render.php
+++ catpow/blocks/loop/render.php
@@ -6,6 +6,9 @@ else{
 	if($attr['query'][0]==='{'){
 		$query=json_decode($attr['query'],1)?:null;
 	}
+	elseif($attr['query'][0]==='[' && preg_match('/^\[([\w_\\\:]+)(\s(.+))?\]$/',$attr['query'],$matches)){
+		$query=$matches[1](shortcode_parse_atts($matches[3]));
+	}
 	else{
 		$query=wp_parse_args($attr['query']);
 	}

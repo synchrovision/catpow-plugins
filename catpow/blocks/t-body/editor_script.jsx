b35d5b3a922f23e8164164daf3c53b037b61e5e0
synchrovision <trinitybluster@yahoo.co.jp>
2020-08-06 14:43:22 +0900

    feat :+1:  add t-body block

---
+++ catpow/blocks/t-body/editor_script.jsx
@@ -0,0 +1,64 @@
+﻿registerBlockType('catpow/t-body',{
+	title:'🐾 T-Body',
+	icon:'editor-code',
+	category:'catpow-mail',
+	attributes:{
+		classes:{source:'attribute',selector:'table',attribute:'class',default:'wp-block-catpow-t-body'}
+	},
+	edit({attributes,className,setAttributes}){
+        const {classes,title}=attributes;
+		const primaryClass='wp-block-catpow-t-body';
+		var states=CP.wordsToFlags(classes);
+		
+		var selectiveClasses=[
+			'color'
+		];
+		
+        return [
+			<table width="100%" className={classes}>
+				<tbody>
+					<tr>
+						<td>
+							<InnerBlocks/>
+						</td>
+					</tr>
+				</tbody>
+			</table>,
+			<InspectorControls>
+				<SelectClassPanel
+					title='クラス'
+					icon='art'
+					set={setAttributes}
+					attr={attributes}
+					selectiveClasses={selectiveClasses}
+					filters={CP.filters['t-body'] || {}}
+				/>
+				<PanelBody title="CLASS" icon="admin-generic" initialOpen={false}>
+					<TextareaControl
+						label='クラス'
+						onChange={(classes)=>setAttributes({classes})}
+						value={classes}
+					/>
+				</PanelBody>
+			</InspectorControls>
+        ];
+    },
+
+
+	save({attributes,className,setAttributes}){
+        const {classes,title}=attributes;
+		const primaryClass='wp-block-catpow-t-body';
+		return (
+			<table width="100%" className={classes}>
+				<tbody>
+					<tr>
+						<td>
+							<InnerBlocks.Content/>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	}
+});
+

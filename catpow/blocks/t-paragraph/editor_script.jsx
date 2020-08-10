b5d394b3a0e127a7c0fdb693fa8a797ec64c10bc
synchrovision <trinitybluster@yahoo.co.jp>
2020-08-10 12:09:36 +0900

    feat :+1:  add t-paragraph block

---
+++ catpow/blocks/t-paragraph/editor_script.jsx
@@ -0,0 +1,93 @@
+﻿registerBlockType('catpow/t-paragraph',{
+	title:'🐾 T-Paragraph',
+	icon:'editor-code',
+	category:'catpow-mail',
+	transforms:{
+		from: [
+			{
+				type:'block',
+				blocks:['core/paragraph'],
+				transform:(attributes)=>{
+					return createBlock('catpow/t-paragraph',{
+						classes:'wp-block-catpow-t-paragraph left medium',
+						text:attributes.content
+					});
+				},
+			},
+			{
+				type:'block',
+				blocks:['catpow/t-heading'],
+				transform:(attributes)=>{
+					return createBlock('catpow/t-paragraph',{
+						classes:'wp-block-catpow-t-paragraph left medium',
+						text:attributes.title
+					});
+				},
+			},
+		]
+	},
+	attributes:{
+		classes:{source:'attribute',selector:'table',attribute:'class',default:'wp-block-catpow-t-paragraph medium'},
+		text:{source:'children',selector:'tbody td',default:'text'}
+	},
+	edit({attributes,className,setAttributes}){
+        const {classes,text}=attributes;
+		const primaryClass='wp-block-catpow-t-paragraph';
+		var states=CP.wordsToFlags(classes);
+		
+		var selectiveClasses=[
+			{label:'サイズ',values:['large','medium','small']}
+		];
+		
+        return [
+			<table width="100%" className={classes}>
+				<tbody>
+					<tr>
+						<td>
+							<RichText
+								onChange={(text)=>{setAttributes({text});}}
+								value={text}
+							/>
+						</td>
+					</tr>
+				</tbody>
+			</table>,
+			<BlockControls>
+				<AlignClassToolbar set={setAttributes} attr={attributes}/>
+			</BlockControls>,
+			<InspectorControls>
+				<SelectClassPanel
+					title='クラス'
+					icon='art'
+					set={setAttributes}
+					attr={attributes}
+					selectiveClasses={selectiveClasses}
+					filters={CP.filters['t-paragraph'] || {}}
+				/>
+				<PanelBody title="CLASS" icon="admin-generic" initialOpen={false}>
+					<TextareaControl
+						label='クラス'
+						onChange={(classes)=>setAttributes({classes})}
+						value={classes}
+					/>
+				</PanelBody>
+			</InspectorControls>
+        ];
+    },
+
+
+	save({attributes,className,setAttributes}){
+        const {classes,text}=attributes;
+		const primaryClass='wp-block-catpow-t-paragraph';
+		return (
+			<table width="100%" className={classes}>
+				<tbody>
+					<tr>
+						<td>{text}</td>
+					</tr>
+				</tbody>
+			</table>
+		);
+	}
+});
+

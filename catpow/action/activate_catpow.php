61ceb52ecc4549ab2fccb68f5384d8094d013c60
synchrovision <trinitybluster@yahoo.co.jp>
2020-08-12 12:40:36 +0900

    feat :+1: add every minutes cron

--- catpow/action/activate_catpow.php
+++ catpow/action/activate_catpow.php
@@ -1,5 +1,8 @@
 <?php
 /*cron*/
+if(!wp_next_scheduled('cp_cron_every_minutes')){
+	wp_schedule_event(ceil(time()/60)*60,'every_minutes','cp_cron_every_minutes');
+}
 if(!wp_next_scheduled('cp_cron_hourly')){
 	wp_schedule_event(ceil(time()/3600)*3600,'hourly','cp_cron_hourly');
 }

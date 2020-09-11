4dd9ebd57885f30a1596728d6e1c03e3384b6d4b
synchrovision <trinitybluster@yahoo.co.jp>
2020-09-11 12:45:44 +0900

    fix :bug: change method value to caps

--- catpow/classes/api.php
+++ catpow/classes/api.php
@@ -6,15 +6,19 @@ namespace Catpow;
 
 abstract class api{
 	public static 
-		$method='get',
+		$method='GET',
 		$check_nonce=false,
 		$capability=false;
 	public static function call($req,$res){
 	}
 	public static function permission($req){
-		if(!empty(static::$method) && $req->get_method!==static::$method){return false;}
+		if(!empty(static::$method) && $req->get_method()!==static::$method){return false;}
 		if(!empty(static::$check_nonce) && wp_verify_nonce($_REQUEST['nonce'],'cp_api')){return false;}
 		if(!empty(static::$capability) && !current_user_can(static::$capability)){return false;}
+		return true;
+	}
+	public static function get_data_name(){
+		return substr(static::class,strrpos(static::class,'\\')+1);
 	}
 }
 

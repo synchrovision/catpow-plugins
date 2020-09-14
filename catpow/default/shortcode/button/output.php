203962ceeceb98a35b451fc313290e95686cbf16
synchrovision <trinitybluster@yahoo.co.jp>
2020-09-14 10:23:50 +0900

    feat :+1: enable to use button shortcode as enclosing shortcode

--- catpow/default/shortcode/button/output.php
+++ catpow/default/shortcode/button/output.php
@@ -1,16 +1,30 @@
 <?php
 namespace Catpow;
-$prm=shortcode_atts([
-	0=>'送信',
-	1=>false,
-	'param'=>null,
-	'target'=>null,
-	'ignore_message'=>null
-],$atts);
-if(isset($prm['param'])){$prm['param']=json_decode($prm['param']);}
-if(!is_null(\cp::$content->form??null)){
-	\cp::$content->form->button($prm[0],$prm[1],$prm['param'],$prm['target'],$prm['ignore_message']);
+if(empty($content)){
+	$prm=shortcode_atts([
+		0=>'送信',
+		1=>false,
+		'param'=>null,
+		'target'=>null,
+		'ignore_message'=>null
+	],$atts);
+	$content=$prm[0];
+	$action=$prm[1];
 }
 else{
-	echo '<a class="button" href="'.$prm[1].'">'.$prm[0].'</a>';
+	$prm=shortcode_atts([
+		0=>false,
+		'param'=>null,
+		'target'=>null,
+		'ignore_message'=>null
+	],$atts);
+	$action=$prm[0];
+}
+if(isset($prm['param'])){$prm['param']=json_decode($prm['param']);}
+
+if(!is_null(\cp::$content->form??null)){
+	\cp::$content->form->button($content,$action,$prm['param'],$prm['target'],$prm['ignore_message']);
+}
+else{
+	echo '<a class="button" href="'.$action.'">'.$content.'</a>';
 }
